d3d7d81d8df7ef74e45404c8170d829f343a877c
yuping-xiao <yupingxiao94@gmail.com>
2020-06-25 15:43:00 +0900

    added slideDown-slideUp function

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 $(function(){
-	$('.box1').hide();
+	$('.box1').slideDown(2000, function() {
+		$('.box1').css({
+			'background-color': '#0000FF',
+			'width': '200px',
+			'height': '100px'
+		}).slideUp(2000);
+	});
 });
