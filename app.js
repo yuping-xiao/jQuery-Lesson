bdfdeebea0dd907bedcb661c0a792f7b843deb02
yuping-xiao <yupingxiao94@gmail.com>
2020-06-25 15:58:47 +0900

    added fadeIn fadeOut function

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 $(function(){
-	$('.box1').slideDown(2000, function() {
-		$('.box1').css({
-			'background-color': '#0000FF',
-			'width': '200px',
-			'height': '100px'
-		}).slideUp(2000);
+	$('.box1').fadeIn(2000, function() {
+		$('.box1').fadeOut(2000, function() {
+			$('.box1').css({
+				'background-color': '#0000FF',
+				'width': '200px',
+				'height': '100px'
+			}).fadeIn(2000);
+			$('.box1').css({
+				'background-color': '#0000FF',
+				'width': '200px',
+				'height': '100px'
+			}).fadeOut(2000);
+		});
 	});
 });
