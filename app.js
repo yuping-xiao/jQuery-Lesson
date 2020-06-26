761be7e94fb769a251892d40bd0f3d6aec22e8f4
yuping-xiao <yupingxiao94@gmail.com>
2020-06-26 22:23:59 +0900

    added addClass removeClass function

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ $(function(){
 		$('.box1').css({'background-color': '#FF0000'});
 	});
 });
+
+$(function(){
+	$('.box1').mouseover(function(){
+		$('.box1').addClass('box1-ext');
+	});
+
+	$('.box1').mouseout(function(){
+		$('.box1').removeClass('box1-ext');
+	});
+});
