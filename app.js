8117a7c01df131ee6c48d190b813506589e452e2
yuping-xiao <yupingxiao94@gmail.com>
2020-06-26 21:51:59 +0900

    added mouseover function

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
 $(function(){
-	$('.box1').fadeIn(2000, function() {
-		$('.box1').fadeOut(2000, function() {
-			$('.box1').css({
-				'background-color': '#0000FF',
-				'width': '200px',
-				'height': '100px'
-			}).fadeIn(2000);
-			$('.box1').css({
-				'background-color': '#0000FF',
-				'width': '200px',
-				'height': '100px'
-			}).fadeOut(2000);
-		});
-	});
+	$('.box1').mouseover(function() {
+			$('.box1').css({'background-color': '#0000FF'});
+			});
 });
